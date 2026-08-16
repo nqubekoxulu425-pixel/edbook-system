e4268b28b18608125782e45f5d8df5720c27d1f4
Nqubeko <nqubekoxulu425-pixel_nqubekoxulu425@gmail.com>
2026-08-16 13:13:47 +0200

    fix netlify routing 404 rules

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Tells the server to redirect 404s back to index.html locally
+  }
 })
